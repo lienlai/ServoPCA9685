641e1f6aab59062e45d2ea93a72a198e4ef7147d
ALI <lienlai@126.com>
2019-10-02 23:02:22 +0800

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,395 @@
+/**
+ * 使用此文件来定义自定义函数和图形块。
+ * 想了解更详细的信息，请前往 https://makecode.microbit.org/blocks/custom
+ */
+ 
+/**
+ * 自定义图形块
+ */
+//% weight=5 color=#0fbc11 icon="\uf113"
+namespace Servo {
+    const PCA9685_ADDRESS = 0x40
+    const MODE1 = 0x00
+    const MODE2 = 0x01
+    const SUBADR1 = 0x02
+    const SUBADR2 = 0x03
+    const SUBADR3 = 0x04
+    const PRESCALE = 0xFE
+    const LED0_ON_L = 0x06
+    const LED0_ON_H = 0x07
+    const LED0_OFF_L = 0x08
+    const LED0_OFF_H = 0x09
+    const ALL_LED_ON_L = 0xFA
+    const ALL_LED_ON_H = 0xFB
+    const ALL_LED_OFF_L = 0xFC
+    const ALL_LED_OFF_H = 0xFD
+ 
+    const STP_CHA_L = 2047
+    const STP_CHA_H = 4095
+ 
+    const STP_CHB_L = 1
+    const STP_CHB_H = 2047
+ 
+    const STP_CHC_L = 1023
+    const STP_CHC_H = 3071
+ 
+    const STP_CHD_L = 3071
+    const STP_CHD_H = 1023
+
+    export enum Motors {
+        M1A = 0x1,
+        M1B = 0x2,
+        M2A = 0x3,
+        M2B = 0x4
+    }
+
+    export enum Steppers {
+        M1 = 0x1,
+        M2 = 0x2
+    }
+
+    export enum Turns {
+        //% blockId="T1B4" block="1/4"
+        T1B4 = 90,
+        //% blockId="T1B2" block="1/2"
+        T1B2 = 180,
+        //% blockId="T1B0" block="1"
+        T1B0 = 360,
+        //% blockId="T2B0" block="2"
+        T2B0 = 720,
+        //% blockId="T3B0" block="3"
+        T3B0 = 1080,
+        //% blockId="T4B0" block="4"
+        T4B0 = 1440,
+        //% blockId="T5B0" block="5"
+        T5B0 = 1800
+    }
+
+    let initialized = false
+ 
+    function i2cwrite(addr: number, reg: number, value: number) {
+        let buf = pins.createBuffer(2)
+        buf[0] = reg
+        buf[1] = value
+        pins.i2cWriteBuffer(addr, buf)
+    }
+
+    function i2ccmd(addr: number, value: number) {
+        let buf = pins.createBuffer(1)
+        buf[0] = value
+        pins.i2cWriteBuffer(addr, buf)
+    }
+ 
+    function i2cread(addr: number, reg: number) {
+        pins.i2cWriteNumber(addr, reg, NumberFormat.UInt8BE);
+        let val = pins.i2cReadNumber(addr, NumberFormat.UInt8BE);
+        return val;
+    }
+ 
+    function initPCA9685(): void {
+        i2cwrite(PCA9685_ADDRESS, MODE1, 0x00)
+        setFreq(50);
+        setPwm(0, 0, 4095);
+        for (let idx = 1; idx < 16; idx++) {
+            setPwm(idx, 0, 0);
+        }
+        initialized = true
+    }
+ 
+    function setFreq(freq: number): void {
+        // Constrain the frequency
+        let prescaleval = 25000000;
+        prescaleval /= 4096;
+        prescaleval /= freq;
+        prescaleval -= 1;
+        let prescale = prescaleval; //Math.Floor(prescaleval + 0.5);
+        let oldmode = i2cread(PCA9685_ADDRESS, MODE1);
+        let newmode = (oldmode & 0x7F) | 0x10; // sleep
+        i2cwrite(PCA9685_ADDRESS, MODE1, newmode); // go to sleep
+        i2cwrite(PCA9685_ADDRESS, PRESCALE, prescale); // set the prescaler
+        i2cwrite(PCA9685_ADDRESS, MODE1, oldmode);
+        control.waitMicros(5000);
+        i2cwrite(PCA9685_ADDRESS, MODE1, oldmode | 0xa1);
+    }
+ 
+    function setPwm(index: number, on: number, off: number): void {
+        if (index < 0 || index > 15)
+            return;
+ 
+        let buf = pins.createBuffer(5);
+        buf[0] = LED0_ON_L + 4 * index;
+        buf[1] = on & 0xff;
+        buf[2] = (on >> 8) & 0xff;
+        buf[3] = off & 0xff;
+        buf[4] = (off >> 8) & 0xff;
+        pins.i2cWriteBuffer(PCA9685_ADDRESS, buf);
+    }
+ 
+    /**
+     * Servo Execute
+     * @param degree [0-180] degree of servo; eg: 90, 0, 180
+    */
+    //% blockId=servo_Servo block="舵机|%index|角度 %degree"
+    //% weight=85
+    //% degree.min=0 degree.max=180
+    export function Servo(index: number,degree: number): void {
+        if (!initialized) {
+            initPCA9685();
+        }
+        // 50hz: 20,000 us
+        let v_us = (degree * 1800 / 180 + 600); // 0.6 ~ 2.4
+        let value = v_us * 4096 / 20000;
+        setPwm(index, 0, value);
+    }
+ 
+    /**
+     * Geek Servo
+     * @param index Servo Channel; eg: S1
+     * @param degree [-45-225] degree of servo; eg: -45, 90, 225
+    */
+    //% blockId=servo_setGeekServo block="Geek舵机|%index|角度 %degree"
+    //% weight=99
+    //% blockGap=50
+    //% degree.min=-45 degree.max=225
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    export function setGeekServo(index: number, degree: number): void {
+        if (!initialized) {
+            initPCA9685()
+        }
+        // 50hz: 20,000 us
+        let v_us = ((degree - 90) * 20 / 3 + 1500) // 0.6 ~ 2.4
+        let value = v_us * 4096 / 20000
+        setPwm(index, 0, value)
+    }
+    
+    /**
+     * Servo Execute
+     * @param pulse [500-2500] pulse of servo; eg: 1500, 500, 2500
+    */
+    //% blockId=servo_ServoPulse block="舵机|%index|脉宽 %pulse"
+    //% weight=85
+    //% pulse.min=500 pulse.max=2500
+    export function ServoPulse(index: number,pulse: number): void {
+        if (!initialized) {
+            initPCA9685();
+        }
+        // 50hz: 20,000 us
+        let value = pulse * 4096 / 20000;
+        setPwm(index, 0, value);
+    }
+    
+    /**
+     * Servo Execute
+     * @param on/off [0-4095] of servo; eg: setPwm(index, 0, 4095)
+    */
+    //% blockId=servo_ServoOnOff block="舵机|%index|on %on|off %off"
+    //% weight=85
+    //% on.min=0 on.max=4095
+    //% off.min=0 off.max=4095
+    export function ServoOnOff(index: number,on: number,off: number): void {
+        if (!initialized) {
+            initPCA9685();
+        }
+        setPwm(index, on, off);
+    }
+
+    
+    /**
+     * Stepper Execute
+    */
+    function Stepper(index: number, dir: boolean): void {
+        if (index == 1) {
+            if (dir) {
+                setPwm(0, STP_CHA_L, STP_CHA_H);
+                setPwm(2, STP_CHB_L, STP_CHB_H);
+                setPwm(1, STP_CHC_L, STP_CHC_H);
+                setPwm(3, STP_CHD_L, STP_CHD_H);
+            } else {
+                setPwm(3, STP_CHA_L, STP_CHA_H);
+                setPwm(1, STP_CHB_L, STP_CHB_H);
+                setPwm(2, STP_CHC_L, STP_CHC_H);
+                setPwm(0, STP_CHD_L, STP_CHD_H);
+            }
+        } else if (index == 2) {
+            if (dir) {
+                setPwm(4, STP_CHA_L, STP_CHA_H);
+                setPwm(6, STP_CHB_L, STP_CHB_H);
+                setPwm(5, STP_CHC_L, STP_CHC_H);
+                setPwm(7, STP_CHD_L, STP_CHD_H);
+            } else {
+                setPwm(7, STP_CHA_L, STP_CHA_H);
+                setPwm(5, STP_CHB_L, STP_CHB_H);
+                setPwm(6, STP_CHC_L, STP_CHC_H);
+                setPwm(4, STP_CHD_L, STP_CHD_H);
+            }
+        }
+    }
+
+    function stopMotor(index: number) {
+        setPwm((index - 1) * 2, 0, 0);
+        setPwm((index - 1) * 2 + 1, 0, 0);
+    }
+
+
+    //% blockId=servo_StepperDegree block="步进电机|%index|角度(负值反转) %degree"
+    //% weight=90
+    export function StepperDegree(index: Steppers, degree: number): void {
+        if (!initialized) {
+            initPCA9685()
+        }
+        Stepper(index, degree > 0);
+        degree = Math.abs(degree);
+        basic.pause(10240 * degree / 360);
+        MotorStopAll()
+    }
+
+    //% blockId=servo_StepperTurn block="步进电机|%index|圈数 %turn"
+    //% weight=90
+    export function StepperTurn(index: Steppers, turn: Turns): void {
+        let degree = turn;
+        StepperDegree(index, degree);
+    }
+
+    //% blockId=servo_StepperDual block="双步进(角度) |M1 %degree1| M2 %degree2"
+    //% weight=89
+    export function StepperDual(degree1: number, degree2: number): void {
+        if (!initialized) {
+            initPCA9685()
+        }
+        Stepper(1, degree1 > 0);
+        Stepper(2, degree2 > 0);
+        degree1 = Math.abs(degree1);
+        degree2 = Math.abs(degree2);
+        basic.pause(10240 * Math.min(degree1, degree2) / 360);
+        if (degree1 > degree2) {
+            stopMotor(3); stopMotor(4);
+            basic.pause(10240 * (degree1 - degree2) / 360);
+        } else {
+            stopMotor(1); stopMotor(2);
+            basic.pause(10240 * (degree2 - degree1) / 360);
+        }
+
+        MotorStopAll()
+    }
+
+    /**
+     * Stepper Car move forward
+     * @param distance Distance to move in cm; eg: 10, 20
+     * @param diameter diameter of wheel in mm; eg: 48
+    */
+    //% blockId=servo_StpCarMove block="步进电机 前进 |距离(mm) %distance|轮直径(mm) %diameter"
+    //% weight=88
+    export function StpCarMove(distance: number, diameter: number): void {
+        if (!initialized) {
+            initPCA9685()
+        }
+        let delay = 10240 * 10 * distance / 3 / diameter; // use 3 instead of pi
+        Stepper(1, delay > 0);
+        Stepper(2, delay > 0);
+        delay = Math.abs(delay);
+        basic.pause(delay);
+        MotorStopAll()
+    }
+
+    /**
+     * Stepper Car turn by degree
+     * @param turn Degree to turn; eg: 90, 180, 360
+     * @param diameter diameter of wheel in mm; eg: 48
+     * @param track track width of car; eg: 125
+    */
+    //% blockId=servo_StpCarTurn block="步进电机 转向 |角度 %turn|轮直径(mm) %diameter|轮距(mm) %track"
+    //% weight=87
+    //% blockGap=50
+    export function StpCarTurn(turn: number, diameter: number, track: number): void {
+        if (!initialized) {
+            initPCA9685()
+        }
+        let delay = 10240 * turn * track / 360 / diameter;
+        Stepper(1, delay < 0);
+        Stepper(2, delay > 0);
+        delay = Math.abs(delay);
+        basic.pause(delay);
+        MotorStopAll()
+    }
+
+    //% blockId=servo_MotorRun block="电机|%index|速度 %speed"
+    //% weight=85
+    //% speed.min=-255 speed.max=255
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    export function MotorRun(index: Motors, speed: number): void {
+        if (!initialized) {
+            initPCA9685()
+        }
+        speed = speed * 16; // map 255 to 4096
+        if (speed >= 4096) {
+            speed = 4095
+        }
+        if (speed <= -4096) {
+            speed = -4095
+        }
+        if (index > 4 || index <= 0)
+            return
+        let pp = (index - 1) * 2
+        let pn = (index - 1) * 2 + 1
+        if (speed >= 0) {
+            setPwm(pp, 0, speed)
+            setPwm(pn, 0, 0)
+        } else {
+            setPwm(pp, 0, 0)
+            setPwm(pn, 0, -speed)
+        }
+    }
+
+
+    /**
+     * Execute two motors at the same time
+     * @param motor1 First Motor; eg: M1A, M1B
+     * @param speed1 [-255-255] speed of motor; eg: 150, -150
+     * @param motor2 Second Motor; eg: M2A, M2B
+     * @param speed2 [-255-255] speed of motor; eg: 150, -150
+    */
+    //% blockId=servo_MotorRunDual block="电机|%motor1|速度 %speed1|%motor2|速度 %speed2"
+    //% weight=84
+    //% speed1.min=-255 speed1.max=255
+    //% speed2.min=-255 speed2.max=255
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    export function MotorRunDual(motor1: Motors, speed1: number, motor2: Motors, speed2: number): void {
+        MotorRun(motor1, speed1);
+        MotorRun(motor2, speed2);
+    }
+
+    /**
+     * Execute single motors with delay
+     * @param index Motor Index; eg: M1A, M1B, M2A, M2B
+     * @param speed [-255-255] speed of motor; eg: 150, -150
+     * @param delay seconde delay to stop; eg: 1
+    */
+    //% blockId=servo_MotorRunDelay block="电机|%index|速度 %speed|延时 %delay|毫秒"
+    //% weight=81
+    //% speed.min=-255 speed.max=255
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    export function MotorRunDelay(index: Motors, speed: number, delay: number): void {
+        MotorRun(index, speed);
+        basic.pause(delay * 1000);
+        MotorRun(index, 0);
+    }
+
+    //% blockId=servo_MotorStop block="电机 停止|%index|"
+    //% weight=80
+    export function MotorStop(index: Motors): void {
+        MotorRun(index, 0);
+    }
+
+    //% blockId=servo_MotorStopAll block="停止所有电机"
+    //% weight=79
+    //% blockGap=50
+    export function MotorStopAll(): void {
+        if (!initialized) {
+            initPCA9685()
+        }
+        for (let idx = 1; idx <= 4; idx++) {
+            stopMotor(idx);
+        }
+    }
+
+}
